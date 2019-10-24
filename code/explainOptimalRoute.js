99c6f32bd68eaee3d4dde1409cc0af4362f50eeb
박길성 <gilseong223@gmail.com>
2019-10-24 10:22:19 +0900

    add: add js

---
+++ code/explainOptimalRoute.js
@@ -0,0 +1,25 @@
+module.exports.function = function explainOptimalRoute (directionRequested, stationRequested) {
+  var routeInfo = []
+
+  var curRouteInfo = {
+
+  }
+  
+  routeInfo.add(curRouteInfo)
+
+  var arrivalInfo = {
+    arrivalTime: arrivalTime,
+    arrivalColor: arrivalColor,
+    arrivalTimeLeft: arrivalTimeLeft
+  }
+
+  var station = {
+    stationName: stationName,
+    arrivalInfo: arrivalInfo
+  }
+
+  return {
+    station: station,
+    routeInfo: routeInfo,
+  }
+}
